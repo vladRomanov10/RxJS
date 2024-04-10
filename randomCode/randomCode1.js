3b964e3452084995e9077f93f896381e62cef656
Vlad Romanov <vladromeur@gmail.com>
2024-04-10 14:24:02 +0500

    Примеры из видео Ильнура

--- randomCode/randomCode1.js
+++ randomCode/randomCode1.js
@@ -16,5 +16,17 @@ c.next(7);
 // Константы
 var e = 5;
 var f = 10;
-var g = new rxjs_1.BehaviorSubject(5).asObservable;
-var h = new rxjs_1.BehaviorSubject(10).asObservable;
+var g = new rxjs_1.BehaviorSubject(5).asObservable(); // метод .asObservable() делает данные (observable) только читаемыми по подписке
+var h = (0, rxjs_1.of)(10); //функция of делает данные (observable) только читаемыми по подписке
+//Создание зависимости одних данных от других
+//На чистом TS или JS это сложная конструкция, которую лучше не делать
+//RxJS
+var sourceData$ = new rxjs_1.BehaviorSubject(5); //это subject - это сущность rxjs, для хранения данных, здесь можно данные хранить и изменять, типо let
+var calculatedData$ = sourceData$.pipe(//это observable(поток) - это сущность rxjs, данные из которых можно только прочитать и то, только с помощью подписки
+(0, rxjs_1.map)(function (value) { return value * 2; }));
+//чтение данных из observable
+calculatedData$.subscribe(//с помощью метода subscribe, куда вставляем колбэк, читаем данные
+function (//с помощью метода subscribe, куда вставляем колбэк, читаем данные
+value) {
+    console.log(value); // получим 10 (5 * 2)
+});
--- randomCode/randomCode1.ts
+++ randomCode/randomCode1.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, of } from 'rxjs'
+import { BehaviorSubject, of, map } from 'rxjs'
 
 //Объявление
 let a: number = 5
@@ -21,5 +21,21 @@ c.next(7)
 const e: number = 5 
 const f: number = 10
 
-const g = new BehaviorSubject(5).asObservable() 
-const h = of(10)
+const g = new BehaviorSubject(5).asObservable() // метод .asObservable() делает данные (observable) только читаемыми по подписке
+const h = of(10) //функция of делает данные (observable) только читаемыми по подписке
+
+//Создание зависимости одних данных от других
+//На чистом TS или JS это сложная конструкция, которую лучше не делать
+//RxJS
+const sourceData$ = new BehaviorSubject(5) //это subject - это сущность rxjs, для хранения данных, здесь можно данные хранить и изменять, типо let
+
+const calculatedData$ = sourceData$.pipe( //это observable(поток) - это сущность rxjs, данные из которых можно только прочитать и то, только с помощью подписки
+    map((value) => value *2)
+)
+
+//чтение данных из observable
+calculatedData$.subscribe( //с помощью метода subscribe, куда вставляем колбэк, читаем данные
+    value => {
+        console.log(value) // получим 10 (5 * 2)
+    }
+);
