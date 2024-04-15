489e17c32ba551769d7cbdde3b7f5be27bd7c702
Vlad Romanov <vladromeur@gmail.com>
2024-04-15 13:36:24 +0500

    прописал метод next в Observable

--- randomCode/observe.ts
+++ randomCode/observe.ts
@@ -1,11 +1,13 @@
 import { Observable } from 'rxjs'
 
 const observable$ = new Observable(observer => {
-
+    observer.next(10)
+    observer.next(20)
+    observer.next(30)
 })
 
 observable$.subscribe({
-    next: () => {},
+    next: (value: number): void => {console.log(value * 2)},
     error: () => {},
     complete: () => {}
 })
