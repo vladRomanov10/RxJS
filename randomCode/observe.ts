37217a70326285751b9592455a0f9ba72f1e9d9c
Vlad Romanov <vladromeur@gmail.com>
2024-04-15 16:04:12 +0500

    прописал метод error в Observable

--- randomCode/observe.ts
+++ randomCode/observe.ts
@@ -2,12 +2,13 @@ import { Observable } from 'rxjs'
 
 const observable$ = new Observable(observer => {
     observer.next(10)
+    observer.error('error')
     observer.next(20)
     observer.next(30)
 })
 
 observable$.subscribe({
     next: (value: number): void => {console.log(value * 2)},
-    error: () => {},
+    error: (err) => {console.log(err)},
     complete: () => {}
 })
