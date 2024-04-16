10ea8e2eb1879b7bb024517b1023a004f579d8af
Vlad Romanov <vladromeur@gmail.com>
2024-04-16 11:16:09 +0500

    Разобрал метод unsubscribe

--- randomCode/observe.ts
+++ randomCode/observe.ts
@@ -1,14 +1,15 @@
 import { Observable } from 'rxjs'
 
 const observable$ = new Observable(observer => {
-    observer.next(10)
-    observer.error('error')
-    observer.next(20)
-    observer.next(30)
+    setTimeout(() => observer.next(10), 1000)
+    setTimeout(() => observer.next(20), 2000)
+    setTimeout(() => observer.next(40), 3000)
 })
 
-observable$.subscribe({
+const subscription = observable$.subscribe({
     next: (value: number): void => {console.log(value * 2)},
     error: (err) => {console.log(err)},
     complete: () => {}
 })
+
+setTimeout(() => subscription.unsubscribe(), 2500)
